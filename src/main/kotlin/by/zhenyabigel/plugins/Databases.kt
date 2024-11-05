855af9bb43557d138b724388553b6d24e4f724bf
Evgeniy <zhenya.bigel@mail.ru>
2024-11-05 03:37:38 +0300

    add database settings

---
+++ src/main/kotlin/by/zhenyabigel/plugins/Databases.kt
@@ -0,0 +1,54 @@
+package by.zhenyabigel.plugins
+
+import by.zhenyabigel.features.authentication.data.model.table.DishTable
+import by.zhenyabigel.features.authentication.data.model.table.UserTable
+import com.typesafe.config.ConfigFactory
+import com.zaxxer.hikari.HikariConfig
+import com.zaxxer.hikari.HikariDataSource
+import io.ktor.server.application.*
+import io.ktor.server.config.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.SchemaUtils
+import org.jetbrains.exposed.sql.transactions.transaction
+
+object DatabaseFactory {
+
+    private val appConfig = HoconApplicationConfig(ConfigFactory.load())
+    private val dbUrl = System.getenv("DB_POSTGRES_URL")
+    private val dbUser = System.getenv("DB_POSTGRES_USER")
+    private val dbPassword = System.getenv("DB_PASSWORD")
+
+    fun Application.initializationDatabase() {
+        Database.connect(getHikariDatasource())
+
+        transaction {
+            SchemaUtils.create(
+                UserTable, DishTable
+            )
+        }
+    }
+
+    private fun getHikariDatasource(): HikariDataSource {
+        println("DB URL: $dbUrl")
+        println("DB USER: $dbUser")
+
+        val config = HikariConfig()
+        config.driverClassName = "org.postgresql.Driver"
+        config.jdbcUrl = dbUrl
+        config.username = dbUser
+        config.password = dbPassword
+        config.maximumPoolSize = 3
+        config.isAutoCommit = false
+        config.transactionIsolation = "TRANSACTION_REPEATABLE_READ"
+        config.validate()
+        return HikariDataSource(config)
+    }
+
+    suspend fun <T> dbQuery(block: () -> T): T {
+        return withContext(Dispatchers.IO) {
+            transaction { block() }
+        }
+    }
+}
